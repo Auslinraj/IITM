d6b7388ae33c12a891392ad93a32c89905744988
Auslin Raj <91816992+Auslinraj@users.noreply.github.com>
2021-10-18 05:56:49 +0000

    operators2

---
+++ OperatorsandExpressions2.py
@@ -0,0 +1,38 @@
+#Arithmetic Operators
+
+print('Addition', 2+3)
+print('SUbraction', 3-2)
+print('Multiplication', 4*4)
+print('Division', 10/2)
+
+print('Floor Division', 32//5)
+
+print('Modulus', 11%2)
+
+print('Exponential', 5**2)
+
+#Relational Operators
+
+print(5==50)
+print(5==5)
+
+print(5!=50)
+print(5!=5)
+
+
+#Logical Operators
+
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+
+print(not (True))
+print(not True)
+print(not(False))
+print(not False)
