8f76f120f0f55e39111b4847667593be36818096
Auslin Raj <91816992+Auslinraj@users.noreply.github.com>
2021-10-18 14:43:05 +0000

    ppa

---
+++ Week1/PPA.py
@@ -0,0 +1,18 @@
+print('15 % 3 * 2 + 2')
+
+print("a + 'b' + c + '' + d")
+
+x = int(input())
+y = int(input())
+z = int(input())
+avg = (x + y + z) / 3
+print(avg)
+
+
+w1 = input()
+w2 = input()
+print(w1 + w2)
+
+2 ** 3 ** 0
+
+2**3**0
---
+++ Week1/PPA1.py
@@ -0,0 +1,45 @@
+#print first five positive integers
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+#print starts
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+
+#Accept an integer as input and print its square as output
+n=int(input())
+m=n*n
+print(m)
+
+#Accept two integers as input and print their sum as output
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
+
+#Accept two words as input and print the two words after adding a space between them
+d=str(input())
+e=str(input())
+f=(d+ ' ' +e)
+print(f)
+
+
+#Accept the registration number of a vehicle as input and print its state-code as output
+RN=input()
+print(RN[0:2])
+
+#Accept a five digit number as input and print the sum of its digits as output.
+n=input()
+n1=int(n[0])
+n2=int(n[1])
+n3=int(n[2])
+n4=int(n[3])
+n5=int(n[4])
+m=n1+n2+n3+n4+n5
+print(m)
