d6d4a30f55b04b5fc5a2f39ef56433dc77b62f8a
Auslin Raj <91816992+Auslinraj@users.noreply.github.com>
2021-10-20 04:45:16 +0000

    escape characters and Quotes

---
+++ Week2/Escape_Characters.py
@@ -0,0 +1,21 @@
+#Escape characters
+print('it\'s a beautiful day')
+print("it's a beautiful day")
+print("we are from \"iit\" Madras")
+print('we are from "iit" Madras')
+print('************')
+
+
+#Additional space
+print('iam auslin, iam from chennai')
+print('iam auslin,    iam from chennai')
+print('iam auslin, \tiam from chennai')
+print('iam auslin, \t\tiam from chennai')
+print('************')
+
+
+#printing in next line
+print('iam auslin.')
+print('iam from chennai.')
+print('iam auslin.\niam from chennai')
+print('************')
---
+++ Week2/Quotes.py
@@ -0,0 +1,25 @@
+#single quote
+x='single quote'
+print(x)
+print('************')
+
+#Double Quotes
+y="Double Quotes"
+print(y)
+print('************')
+
+#Triple Quotes
+z='''Triple Quotes'''
+print(z)
+z='''line1
+line2
+line3
+line4'''
+print(z)
+
+#Comment
+#Comment1
+'''this
+also used as
+comment, if not
+assighned to variable'''
