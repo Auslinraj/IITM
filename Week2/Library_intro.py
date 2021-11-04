b514e02d225489f6bbb6b07fb8975b67f5292049
Auslin Raj <91816992+Auslinraj@users.noreply.github.com>
2021-11-04 12:57:57 +0000

    library methods

--- Week2/Library_intro.py
+++ Week2/Library_intro.py
@@ -1,6 +1,38 @@
+#import library math
+
 import math
 print(math.sqrt(9))
 print(math.factorial(5))
 
+#import library random
+
 import random
 print(random.random())
+
+#Import library calendar
+
+import calendar
+print(calendar.calendar(2021))
+print(calendar.month(2021,7))
+
+#another way of importing calendar
+
+from calendar import *
+print(month(2021,10))
+print(calendar(2021))
+
+#Import only month from calendar
+
+from calendar import month
+print(month(2021,8))
+
+#import library as variable
+
+import calendar as c
+print(c.calendar(2021))
+print(c.month(2021,5))
+
+#import part of library as variable
+
+from calendar import month as m
+print(m(2021,6))
