89612eb474c0553ce24d6721d1798e4879822c79
Auslin Raj <91816992+Auslinraj@users.noreply.github.com>
2021-11-09 00:15:43 +0000

    while

---
+++ Week3/While.py
@@ -0,0 +1,11 @@
+#Introduction to while loop
+#loops until it gets the right Answer
+
+print("When India get its Independence?")
+year=int(input())
+
+while(year!=1947):
+  print("wrong!!!, Try again")
+  year=int(input())
+
+print('Wowwww....')
---
+++ Week3/While_Factorial.py
@@ -0,0 +1,13 @@
+#Find Factorail of a Number
+
+print('Enter a Number: ')
+n=int(input())
+
+i=1
+ans=1
+
+while(i<=n):
+  ans=ans*i
+  i=i+1
+
+print(ans)
