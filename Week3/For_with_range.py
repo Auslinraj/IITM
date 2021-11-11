f80fddefbdf4dd99f476a0a410be17b64605b5fd
Auslin Raj <91816992+Auslinraj@users.noreply.github.com>
2021-11-11 05:15:18 +0000

    for

---
+++ Week3/For_with_range.py
@@ -0,0 +1,21 @@
+#for loop with range
+
+for x in range(1,11):
+  print(x)
+
+#for loop with range and step
+
+for x in range(1,11,2):
+  print(x)
+
+for x in range(1,11,3):
+  print(x)
+
+#printing in reverse
+
+for x in range(10,-1,-1):
+  print(x)
+
+
+for x in range(9,-1,-2):
+  print(x)
---
+++ Week3/For_without_range.py
@@ -0,0 +1,11 @@
+#for loop without range
+
+country='India'
+for letter in country:
+  print(country)
+
+
+
+country='India'
+for letter in country:
+  print(letter)
